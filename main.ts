c37a60c4d7a053df7fc0273417c79f8508c76b7c
Alexander Welsing <kontakt@a-wels.de>
2024-09-02 14:40:14 +0000

    initial code

--- main.ts
+++ main.ts
@@ -1,134 +1,134 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { NodeSSH } from 'node-ssh';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface MyScpPluginSettings {
+  serverAddress: string;
+  username: string;
+  password: string;
+  remotePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+
+const DEFAULT_SETTINGS: MyScpPluginSettings = {
+  serverAddress: '',
+  username: '',
+  password: '',
+  remotePath: ''
+};
+
+export default class MyScpPlugin extends Plugin {
+  settings: MyScpPluginSettings;
+
+  async onload() {
+    await this.loadSettings();
+
+    this.addCommand({
+      id: 'upload-current-file',
+      name: 'Upload Current File via SCP',
+      callback: () => this.uploadCurrentFile(),
+    });
+
+    this.addSettingTab(new MyScpPluginSettingTab(this.app, this));
+  }
+
+  async uploadCurrentFile() {
+    const activeFile = this.app.workspace.getActiveFile();
+    if (!activeFile) {
+      console.error('No active file');
+      return;
+    }
+
+    const content = await this.app.vault.read(activeFile);
+    const ssh = new NodeSSH();
+
+    try {
+      await ssh.connect({
+        host: this.settings.serverAddress,
+        username: this.settings.username,
+        password: this.settings.password,
+      });
+	  // Create directory if it doesn't exist
+	  await ssh.execCommand(`mkdir -p ${this.settings.remotePath}`);
+      await ssh.putFile(content, `${this.settings.remotePath}/${activeFile.name}`);
+      console.log(`File ${activeFile.name} uploaded successfully`);
+    } catch (err) {
+      console.error('Error uploading file:', err);
+    } finally {
+      ssh.dispose();
+    }
+  }
+
+  onunload() {
+    console.log('Unloading MyScpPlugin');
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class MyScpPluginSettingTab extends PluginSettingTab {
+  plugin: MyScpPlugin;
 
-	async onload() {
-		await this.loadSettings();
+  constructor(app: any, plugin: MyScpPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  display(): void {
+    const { containerEl } = this;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    containerEl.empty();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    containerEl.createEl('h2', { text: 'Settings for SCP Upload Plugin' });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    new Setting(containerEl)
+      .setName('Server Address')
+      .setDesc('The address of the server to upload files to.')
+      .addText(text => text
+        .setPlaceholder('example.com')
+        .setValue(this.plugin.settings.serverAddress)
+        .onChange(async (value) => {
+          this.plugin.settings.serverAddress = value;
+          await this.plugin.saveSettings();
+        }));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    new Setting(containerEl)
+      .setName('Username')
+      .setDesc('The username for SCP authentication.')
+      .addText(text => text
+        .setPlaceholder('your-username')
+        .setValue(this.plugin.settings.username)
+        .onChange(async (value) => {
+          this.plugin.settings.username = value;
+          await this.plugin.saveSettings();
+        }));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    new Setting(containerEl)
+      .setName('Password')
+      .setDesc('The password for SCP authentication.')
+      .addText(text => text
+        .setPlaceholder('your-password')
+        .setValue(this.plugin.settings.password)
+        .onChange(async (value) => {
+          this.plugin.settings.password = value;
+          await this.plugin.saveSettings();
+        }));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
+		// Target path for uploaded files
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Remote Path')
+			.setDesc('The path on the remote server to upload files to.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('/path/on/remote/server')
+				.setValue(this.plugin.settings.remotePath)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+					this.plugin.settings.remotePath = value;
+					await this.plugin.saveSettings
+  }
+
 }
